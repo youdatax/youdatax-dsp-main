456a5c48921a996fa5a96d95a4d041180a1de83b
yanggoat <yanggoat@outlook.com>
2026-09-21 12:24:09 +0800

    add pom for project youdatax-dsp-main-service-http

--- youdatax-dsp-main-service-http/build.gradle.kts
+++ youdatax-dsp-main-service-http/build.gradle.kts
@@ -9,7 +9,7 @@
  *
  *  Contributors:
  *       Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V. - initial API and implementation
- *
+ * \data-protocols\dsp\dsp-core\dsp-http-core
  */
 
 plugins {
